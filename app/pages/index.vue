fe94f407da125e3ff62ad369bc15027153e65e0d
panwj <panwj@xunbai.onaliyun.com>
2021-12-14 15:45:39 +0800

    xx

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
-        <div class="mb-12 xl:mb-0">
+        <div class="mb-12  xl:mb-0">
           <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
 
           <form
